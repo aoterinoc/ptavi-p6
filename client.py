a4a7f12de873bc171f8114757b7af83b704e1b44
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-11-17 10:53:26 +0100

    ejecucion cliente

--- client.py
+++ client.py
@@ -5,26 +5,43 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+if len(sys.argv) != 3:
+    print "Usage : python client.py method receiver@IP:SIPport"
+    sys.exit()
+METODO = sys.argv[1]
+DATOS = sys.argv[2]
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+#Troceo argv[2] para obtener el server&port
+DATOS = DATOS.split("@")
+DATOS[1] = DATOS[1].split(":")
+SERVER = DATOS[1][0]
+PORT = int(DATOS[1][1])
+RECEPTOR = DATOS[0]
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+
+
+
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
+LINE = METODO.upper() + " sip:" + RECEPTOR+ "@" + SERVER + " SIP/2.0"
+print LINE
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
+#Excepcion en caso de establecer conexion con un puerto no abierto
+try:
+    data = my_socket.recv(1024)
+    print 'Recibido -- ', data
+except (socket.error):
+    print "No server listening at " + str(SERVER) + " port " + str(PORT)
+    sys.exit()
+
 print "Terminando socket..."
 
 # Cerramos todo
