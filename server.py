a5659ebf4823d676af7727f30501499a4aaa4dc4
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-11-24 10:13:55 +0100

    pep8

--- server.py
+++ server.py
@@ -9,7 +9,9 @@ import sys
 import os.path
 import os
 
-METODOS_ACEPTADOS = ["INVITE","BYE","ACK"]
+
+METODOS_ACEPTADOS = ["INVITE", "BYE", "ACK"]
+
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -19,23 +21,23 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     def handle(self):
         while 1:
             # Lee linea a lina lo que llega del cliente
-          
+
             line = self.rfile.read()
-            
+
             #Comprobamos si hay linea en blanco
-            if not line: 
+            if not line:
                 break
             print "Recibo del cliente"
             print line
             if line != "":
                 sip = "SIP/2.0\r\n\r\n"
                 #miro aqui el codigo 400 xq el split me corta los \r\n
-                if 'sip:' not in line or sip not in line or "@" not in line: 
+                if 'sip:' not in line or sip not in line or "@" not in line:
                     self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
-                    break # SI ME MANDA MAL EL MENSAJE YA NO LE MANDO NADA MAS!!!!!!!!!!!!!!
+                    break
+                    #Si el cliente me manda mal el mensaje no le mando nada mas
                 #troceamos la linea que nos llega
                 line = line.split()
-              
                 if line[0] == "INVITE":
                     trying = "SIP/2.0 100 Trying\r\n\r\n"
                     ring = "SIP/2.0 180 Ring\r\n\r\n"
@@ -43,29 +45,25 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                     cod_respuesta = trying + ring + ok
                     self.wfile.write(cod_respuesta)
                 if line[0] not in METODOS_ACEPTADOS:
-                    self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
-                
+                    self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")       
                 if line[0] == "ACK":
                     print "Me llega ack envio RTP"
-                    aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 <' + FICHERO_AUDIO    
+                    aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 <' + FICHERO_AUDIO
                     os.system(aEjecutar)
                     print "Se acaba la transmision de RTP"
-                if line[0] == "BYE":    
+                if line[0] == "BYE":
                     self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-               
 
 if __name__ == "__main__":
-   
-    if len(sys.argv) != 4 or os.path.exists(sys.argv[3]) == False:
+
+    if len(sys.argv) != 4 or os.path.exists(sys.argv[3]) is False:
         print "Usage : python server.py IP port audio_file"
         sys.exit()
- 
+
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
     FICHERO_AUDIO = sys.argv[3]
-    
-  
-   
+
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", PORT), EchoHandler)
     print "Listening..."
