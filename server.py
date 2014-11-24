19d576eabe3dd3f983e735e52a78fed5e1247bc0
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-11-24 09:22:48 +0100

    RTP

--- server.py
+++ server.py
@@ -21,41 +21,38 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             # Lee linea a lina lo que llega del cliente
           
             line = self.rfile.read()
+            
+            #Comprobamos si hay linea en blanco
+            if not line: 
+                break
             print "Recibo del cliente"
             print line
-            #Comprobamos si hay linea en blanco
-            if not line:
-                break
-            sip = "SIP/2.0\r\n\r\n"
-            #miro aqui el codigo 400 xq el split me corta los \r\n
-            if 'sip:' not in line or sip not in line or "@" not in line: #volver a mirar esto bien,incluir@!!!!!!!!!!!!!!!!
-                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
-                sys.exit()
-            #troceamos la linea que nos llega
-            line = line.split()
-            
-            #line[1] = line[1].split(":")
-            print "LINEA TROCEADA"
-            print line
-            
-            if line[0] == "INVITE":
-                trying = "SIP/2.0 100 Trying\r\n\r\n"
-                ring = "SIP/2.0 180 Ring\r\n\r\n"
-                ok = "SIP/2.0 200 OK\r\n\r\n"
-                cod_respuesta = trying + ring + ok
-                self.wfile.write(cod_respuesta)
-            if line[0] not in METODOS_ACEPTADOS:
-                self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
-            
-            if line[0] == "ACK":
-                print "me llega ACK,envio RTP"
-                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 <' + FICHERO_AUDIO
-                os.system(aEjecutar)
-            if line[0] == "BYE":    
-                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+            if line != "":
+                sip = "SIP/2.0\r\n\r\n"
+                #miro aqui el codigo 400 xq el split me corta los \r\n
+                if 'sip:' not in line or sip not in line or "@" not in line: 
+                    self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
+                    break # SI ME MANDA MAL EL MENSAJE YA NO LE MANDO NADA MAS!!!!!!!!!!!!!!
+                #troceamos la linea que nos llega
+                line = line.split()
+              
+                if line[0] == "INVITE":
+                    trying = "SIP/2.0 100 Trying\r\n\r\n"
+                    ring = "SIP/2.0 180 Ring\r\n\r\n"
+                    ok = "SIP/2.0 200 OK\r\n\r\n"
+                    cod_respuesta = trying + ring + ok
+                    self.wfile.write(cod_respuesta)
+                if line[0] not in METODOS_ACEPTADOS:
+                    self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                
+                if line[0] == "ACK":
+                    print "Me llega ack envio RTP"
+                    aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 <' + FICHERO_AUDIO    
+                    os.system(aEjecutar)
+                    print "Se acaba la transmision de RTP"
+                if line[0] == "BYE":    
+                    self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+               
 
 if __name__ == "__main__":
    
